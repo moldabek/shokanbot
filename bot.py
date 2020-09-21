3bae98a7130883cdedea327890ac304cedab5572
Moldabekov Beibarys <biba4756@gmail.com>
2020-09-21 12:51:31 +0600

    Change models

--- bot.py
+++ bot.py
@@ -1,20 +1,18 @@
+import threading
 import telebot
 import time
 from telebot import types
 import logging
 from datetime import datetime
-from pymongo import MongoClient
-from config import TOKEN, MONGO
-from data.models import Session, Users
-import schedule
-
+from config import TOKEN
+from data.models import Session
+from sqlalchemy import table, column
 
 FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 logging.basicConfig(filename='sample.log', level=logging.ERROR, format=FORMAT)
 bot = telebot.TeleBot(TOKEN)
-client = MongoClient(MONGO)
-db = client.test
-users = db['users']
+users = {'bilbao': '26'}
+
 keyboard1 = types.ReplyKeyboardMarkup()
 keyboard2 = types.KeyboardButton('Доброе утро')
 keyboard1.add(keyboard2)
@@ -25,35 +23,28 @@ n = 0
 @bot.message_handler(commands=['start'])
 def start_message(message):
     # now = datetime.now()
-    msg = bot.send_message(message.chat.id, "Привет, отправь логин")
+    msg = bot.send_message(message.chat.id, "Введите логин: ")
     # point = 100
     bot.register_next_step_handler(msg, login)
 
 
 def login(message):
-    postgres = Session()
     try:
-        obj = postgres.query(Users).filter(Users.login == str(message.text)).one()
-        print(obj)
-        postgres.close()
-        msg = bot.send_message(message.chat.id, f'Please, enter your password')
-        bot.register_next_step_handler(msg, pas)
-        # data = message.text.split()
-        # print(data)
-        # print(data[0])
-        # print(data[1])
-        # check = users.find_one({
-        #     'username': data[0],
-        #     'password': data[1],
-        # })
-        #
-        # if check is None:
-        #     msg = bot.send_message(message.chat.id, r'Неправильно введен логин\пароль')
-        #     bot.send_message(message.chat.id, "Please, send me again your login & pass")
-        #     bot.register_next_step_handler(msg, login)
-        # else:
-        #     msg = bot.send_message(message.chat.id, 'Good morning', reply_markup=keyboard1)
-        #     bot.register_next_step_handler(msg, vic)
+        # obj = postgres.query(Users).filter(Users.login == str(message.text)).one()
+        # print(obj)
+        # postgres.close()
+        data = message.text.split()
+        print(data)
+        print(data[0])
+        print(data[1])
+
+        if users[data[0]] != data[1]:
+            msg = bot.send_message(message.chat.id, r'Неправильно введен логин\пароль')
+            bot.register_next_step_handler(msg, login)
+        else:
+            msg = bot.send_message(message.chat.id, 'Good morning', reply_markup=keyboard1)
+            bot.register_next_step_handler(msg, vic)
+
     except Exception as e:
         logging.error("Exception")
         print(e)
@@ -61,40 +52,38 @@ def login(message):
         bot.register_next_step_handler(msg, login)
 
 
-def pas(message):
-    post = Session()
-    try:
-        obj = post.query(Users).filter(Users.password == str(message.text)).one()
-        print(obj)
-        post.close()
-        msg = bot.send_message(message.chat.id, 'Good morning', reply_markup=keyboard1)
-        bot.register_next_step_handler(msg, vic)
-    except Exception as e:
-        logging.error(Exception)
-        print(e)
-        msg = bot.send_message(message.chat.id, "Your password doesn't correct please send again")
-        bot.register_next_step_handler(msg, pas)
-
-
-def vic(message):
-    now = datetime.now()
-    msg = bot.send_message(message.chat.id, "Ответьте на вопрос после 20-ти секунд\nГлубина Марианской впдадины?")
-    send = bot.send_message(message.chat.id, 'Осталось : 20')
+def timer(chat_id, message_id):
     for i in range(19, -1, -1):
         time.sleep(1)
-        bot.edit_message_text(f'Осталось : {i}', message.chat.id, send.message_id)
-    bot.send_message(message.chat.id, "Жду ответа :)")
-    bot.register_next_step_handler(msg, ans)
+        bot.edit_message_text(f'Осталось : {i}', chat_id, message_id)
 
 
 def ans(message):
-    bot.send_message(message.chat.id, "Ответ принят")
-    answer = message.text
-    print(answer)
+    print(message)
+    print("check")
+    if message.text == "15":
+        bot.send_message(message.chat.id, "Верно!")
+        msg = bot.send_message(message.chat.id, "Отправьте скрин инста-сториза :)")
+        bot.register_next_step_handler(msg, screenshotcheker)
+
+
+def vic(message):
+    bot.send_message(message.chat.id, "Ответьте на вопрос в течении 20-ти секунд\nГлубина Марианской впдадины?")
+    send = bot.send_message(message.chat.id, 'Осталось : 20')
+    x = threading.Thread(target=timer, args=(message.chat.id, send.message_id))
+    x.start()
+    bot.register_next_step_handler(send, ans)
 
 
 @bot.message_handler(content_types=['photo', 'file'])
 def screenshotcheker(message):
+    postgres = Session()
+    score = 100
+    now = datetime.now()
+    username = message.from_user.username
+    chat_id = message.chat.id
+    t = table('scores', column("id"), column("username"), column("score"))
+    t.insert().values(id=chat_id, username=f'{username}', score=score)
     bot.send_message(message.chat.id, "Thank you for using our bot")
 
 
--- data/models.py
+++ data/models.py
@@ -1,9 +1,10 @@
-from sqlalchemy import create_engine, MetaData
+from sqlalchemy import create_engine, MetaData, ForeignKey, text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime
+from config import url
 
-url = 'postgresql://postgres:biba01@localhost:5432/shokan'
+url = url
 engine = create_engine(url, client_encoding='UTF-8', echo=True)
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
@@ -12,10 +13,18 @@ meta = MetaData()
 
 class Users(Base):
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, nullable=False)
     username = Column(String)
     login = Column(String)
-    password = Column(String)
+
+
+class Score(Base):
+    __tablename__ = 'scores'
+    id = Column(Integer, primary_key=True, nullable=False)
+    username = Column(String, nullable=False)
+    score = Column(Integer)
+    date = Column(DateTime, default=text('NOW()'))
+    user = Column(ForeignKey(Users))
 
 
 def create_table():
